2c479271234e43a4804e9d4421dff659475ea17d
Eamon White <eamon.white7@gmail.com>
2017-04-24 16:05:14 -0400

    fixed indexjs

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'com.eamon.sneek',
   masterKey: process.env.MASTER_KEY || 'blahjour7', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://parse-eamonbenproject.rhcloud.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost.com:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
